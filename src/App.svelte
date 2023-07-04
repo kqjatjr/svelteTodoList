aa5335c1f491f6918a9ea0f525957589bcdcb1e7
bskim <bskim2@rsupport.com>
2023-07-04 23:26:48 +0900

    추가 삭제 구현

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,42 @@
 
 <script lang="ts">
   import Input from "./components/Input.svelte";
+  import Todos from "./components/Todos.svelte";
+
+  let todoValue = "";	
+  let todoList = [];
+
+  let lastId = 0;
+
+  const addTodo = () => {
+      if(todoValue) {
+        const newTodo = {
+          id: ++lastId,
+          text: todoValue,
+          completed: false
+        }
+        todoList = [...todoList, newTodo];
+        todoValue = "";
+      }
+	}
+
+  const removeTodo = (id: string) => {
+    todoList = todoList.filter((prev) => prev.id !== id)
+  }
+	
+	const handleKeyUp = e => {
+		todoValue = e.target.value;
+		if(e.keyCode === 13) {
+			addTodo();
+		}
+	}
 </script>
 
 <main class="main-container">
   <div class="title">Todo List</div>
-  <Input />
+  <Input {todoValue} {addTodo} {handleKeyUp}/>
+  <Todos {todoList} {removeTodo}/>
 </main>
-
 
 <style>
   .title {
