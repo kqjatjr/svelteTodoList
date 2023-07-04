217316334847e0c0d615f07fb797ab14bb5fb492
bskim <bskim2@rsupport.com>
2023-07-04 21:12:43 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Input from "./components/Input.svelte";
+</script>
+
+<main>
+  <div>Todo List</div>
+  <Input />
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/components/Input.svelte
@@ -0,0 +1,9 @@
+<input class="todo-input" placeholder="할일을 입력하세요"/>
+<button>추가</button>
+
+
+<style>
+  .todo-input {
+    width: 20px;
+  }
+</style>
