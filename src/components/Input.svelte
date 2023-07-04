138fbc07f496fddf2dc7f35961de9cb3d972af31
bskim <bskim2@rsupport.com>
2023-07-04 22:04:28 +0900

    기초 css 추가

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,9 +1,29 @@
-<input class="todo-input" placeholder="할일을 입력하세요"/>
-<button>추가</button>
+<!-- <input class="todo-input" placeholder="할일을 입력하세요"/>
+<button class="todo-add-button">추가</button> -->
+<script>
+  
+</script>
 
+
+<div>
+  <input class="todo-input" placeholder="할일을 입력하세요"/>
+  <button class="todo-button">추가</button>
+</div>
 
 <style>
   .todo-input {
-    width: 20px;
+    width: 300px;
+    height: 50px;
+    box-sizing: border-box;
+    border-radius: 25px;
+    padding-left: 10px;
+  }
+
+  .todo-button {
+    height: 50px;
+    width: 80px;
+    box-sizing: border-box;
+    border-radius: 25px;
+    font-size: 20px;
   }
 </style>
